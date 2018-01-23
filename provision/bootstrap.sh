36ef68afc870af7d56f70f62d21e15fe2a90a84d
Marko Kosir <kosir.mar@gmail.com>
2018-01-23 22:14:10 +0100

    -

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -17,7 +17,8 @@ main() {
 	apacheConfig
 	phpConfig
 	composerConfig
-	#mysqlConfig
+	nodejsConfig
+	#mysqlConfigs
 	mariaDBConfig
 	restartServices
 	cleanUp
@@ -99,6 +100,18 @@ composerConfig() {
 
     printf "\n"
     composer --version
+}
+
+nodejsConfig() {
+    ###### Node.js & npm
+    printf "\n\n\n\n[ #### Install Node.js & npm #### ]\n\n"
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+
+    printf "\n"
+    node -v
+    printf "\n"
+    npm -v
 }
 
 mysqlConfig() {
